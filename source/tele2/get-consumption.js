8e33efecfebd1250529b3568c2470406a61f438a
rogerbf <roger@bergfalk.nu>
2017-02-01 15:07:05 +0100

    refactor

--- source/tele2/get-consumption.js
+++ source/tele2/get-consumption.js
@@ -1,12 +1,16 @@
-export default ({ nightmare }) =>
+export default ({ nightmare, extracted, ...configuration }) =>
   nightmare
   .goto(`https://www.tele2.se/mitt-tele2/forbrukning`)
   .wait(`.t2-usage`)
   .goto(`https://www.tele2.se/t2api/consumptions/GetBucketList`)
   .evaluate(() => document.querySelector(`body`).innerText)
-  .then(consumptionData => {
-    Object.assign(nightmare, { extracted: JSON.parse(consumptionData) })
-  })
-  .then(() => {
-    return { nightmare }
-  })
+  // .then(consumptionData => {
+  //   Object.assign(nightmare, { extracted: JSON.parse(consumptionData) })
+  // })
+  // .then(() => {
+  //   return { nightmare }
+  // })
+  .then(data => ({
+    nightmare,
+    extracted: { ...extracted, consumption: JSON.parse(data) }
+  }))
