1b39a1148d300ab034eba9332e915dcf2e60f1ee
rogerbf <roger@bergfalk.nu>
2017-05-29 21:52:02 +0200

    initial

---
+++ source/tele2/get-subscriptions.js
@@ -0,0 +1,13 @@
+// #subscriptions
+//   li
+//     .t2-number
+//     [href*=Consumption]
+
+// {
+//   subscriptions: [
+//     {
+//       number: 123456789,
+//       consumptionUrl: `/mitt-tele2/RedirectToConsumptionPage?msisdn=46123456789`
+//     }
+//   ]
+// }
