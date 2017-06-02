b1b5f546b94b96c4f47531f44b1f5c0867e551ed
rogerbf <roger@bergfalk.nu>
2017-06-02 22:58:53 +0200

    experimenting

--- source/index.js
+++ source/index.js
@@ -1,33 +1,38 @@
-const nightmare = require(`nightmare`)
-
-const debug = process.env.DEBUG === `consumption`
-
-const nightmareOptions = {
-  show: debug,
-  typeInterval: 1,
-  waitTimeout: 45000,
-  gotoTimeout: 45000
+const configuration = {
+  dependencies: {
+    nightmare: require(`nightmare`)
+  },
+  debug: process.env.DEBUG === `consumption`,
+  nightmareOptions: {
+    typeInterval: 1,
+    waitTimeout: 45000,
+    gotoTimeout: 45000
+  },
+  useragent: (
+    process.env.NODE_ENV === `development`
+    ? `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36`
+    : `https://www.npmjs.com/package/consumption`
+  )
 }
 
-const tele2 = (
-  credentials = { email: undefined, password: undefined },
-  state = {
-    credentials,
-    nightmare: (
-      nightmare(nightmareOptions)
-      .useragent(
-        debug
-        ? `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36`
-        : `https://www.npmjs.com/package/consumption`
+const initialize = (
+  configuration,
+  { dependencies: { nightmare }, debug, nightmareDefaultOptions, useragent }
+) =>
+  Object.assign(
+    {},
+    configuration,
+    {
+      scraper: (
+        nightmare(Object.assign({}, nightmareDefaultOptions, { show: debug }))
+        .useragent(useragent)
       )
-    )
-  }
-) => {
-}
+    }
+  )
 
 // login
 // fetch subscriptions
 // select subscription, default to all
 // get consumption
 
-module.exports = tele2
+module.exports = () => {}
